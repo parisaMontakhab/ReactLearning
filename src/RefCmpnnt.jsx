8af2743854bf8e7923d42455a3dc94d2caca8627
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-04-26 11:11:16 +0200

    feat: add useeffect in refcomponent for learning useref

--- src/RefCmpnnt.jsx
+++ src/RefCmpnnt.jsx
@@ -1,9 +1,11 @@
-import { useRef } from "react"
+import { useEffect, useRef } from "react"
 
 
 export default function RefCmpnnt() {
     const refValue=useRef();
-    
+    useEffect(()=>{
+        refValue.current.focus();
+    },[])
   return (
     <div>
         <input placeholder="search" ref={refValue}/>
