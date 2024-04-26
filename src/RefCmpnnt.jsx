f6f3a58d96b62c489e7faacfd0be8859b930d9be
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-04-26 11:40:43 +0200

    refactor: commented all code about refcomponnt for writing another code

--- src/RefCmpnnt.jsx
+++ src/RefCmpnnt.jsx
@@ -2,23 +2,23 @@ import { useEffect, useRef } from "react"
 
 
 export default function RefCmpnnt() {
-    const refValue=useRef();
+    // const refValue=useRef();
 
     // useEffect(()=>{
     //     refValue.current.focus();
     //     refValue.current.value ="test value"
     // },[])
 
-    function handelFocus(){
-        refValue.current.focus();
-        refValue.current.value ="search here"
+//     function handelFocus(){
+//         refValue.current.focus();
+//         refValue.current.value ="search here"
         
-    }
+//     }
 
-  return (
-    <div>
-        <input  ref={refValue}/>
-        <button onClick={handelFocus}>Click to focus</button>
-    </div>
-  )
+//   return (
+//     <div>
+//         <input  ref={refValue}/>
+//         <button onClick={handelFocus}>Click to focus</button>
+//     </div>
+//   )
 }
