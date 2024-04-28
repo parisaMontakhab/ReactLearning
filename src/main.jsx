72d9be55e9d9fdc6ef0c1169830fa453b64fd5b6
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-04-28 19:53:58 +0200

    feat: render inputparent component in main component

--- src/main.jsx
+++ src/main.jsx
@@ -13,13 +13,14 @@ import {
   IsertList,ReversList, InputForm
 } from "./Form";
 import RefCmpnnt from "./RefCmpnnt";
-import { Timer } from "./RefCmpnnt";
+import { Timer,InputParent } from "./RefCmpnnt";
 
 export default function App() {
   return (
     <div>
 <RefCmpnnt/>
 <Timer/>
+<InputParent/>
       
     </div>
   );
