70678733a2c6a022b096ce6f09d9e606f22ae20a
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-04-29 14:24:55 +0200

    feat: add function onclick about inc,dec,res

--- src/RedusUse.jsx
+++ src/RedusUse.jsx
@@ -1,14 +1,26 @@
 import React, { useState } from "react";
 const intialState = 0;
 export default function RedusUse() {
-    const[count,setCount] = useState(intialState);
+  const [count, setCount] = useState(intialState);
+
+  function handleInc() {
+    setCount((c)=> c+1);
+  }
+  function handleRes() {
+    setCount(intialState);
+  }
+  function handleDec() {
+    setCount((c)=> c-1);
+  }
   return (
     <div>
-      <div> count:{count}
-      <br/>
-        <button>+</button>
-        <button>reset</button>
-        <button>-</button>
+      <div>
+        {" "}
+        count:{count}
+        <br />
+        <button onClick={handleInc}>+</button>
+        <button onClick={handleRes}>reset</button>
+        <button onClick={handleDec}>-</button>
       </div>
     </div>
   );
